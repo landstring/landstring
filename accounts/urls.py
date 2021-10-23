bc77ef7354a6c3021a3a250f281c4484821a77e7
Yaroslav Bujkevich <81531853+landstring@users.noreply.github.com>
2021-10-23 23:58:51 +0400

    Revert "Merge branch 'main' of https://github.com/landstring/landstring"
    This reverts commit c9c18d52da94cd40cce89f6ea116a47ae101a871, reversing
    changes made to 2961494c3777d218447c3d3c1d098dacc4d4a174.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,30 @@ urlpatterns = [
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
+
+
     path('', views.home, name="home"),
+
+    path('startups/', views.startups, name="startups"),
+    path('my_startups/', views.my_startups, name="my_startups"),
+    path('my_startups/<int:id>/', views.startupView),
+    path('new_startup/', views.new_startup, name="new_startup"),
+    path('startups/<int:id>/', views.startupView),
+
+    path('projects/', views.projects, name="projects"),
+    path('new_project/', views.new_project, name="new_project"),
+
+    path('teams/', views.teams, name="teams"),
+    path('my_teams/', views.my_teams, name="my_teams"),
+    path('new_team/', views.new_team, name="new_team"),
+
+    path('articles/', views.articles, name="articles"),
+    path('my_articles/', views.my_articles, name="my_articles"),
+    path('new_articles/', views.new_articles, name="new_articles"),
+
+    path('learning/', views.learning, name="learning"),
+
+
+    path('account/', views.account, name="account"),
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
